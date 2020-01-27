eb2d14c0cb98fe37eebea170a48c9b477d1ac823
Alberto Redondo <alberto.redondo@icmat.es>
2020-01-27 19:23:53 +0100

    Added predict_heuristic_par function

--- src/inference.py
+++ src/inference.py
@@ -1,9 +1,11 @@
+
 import numpy as np
 import pandas as pd
 from data import *
 from models import *
 from samplers import *
 from sklearn.linear_model import LogisticRegression
+from joblib import Parallel, delayed
 
 '''
 Inference functions for the ARA approach to Adversarial Classification
@@ -35,6 +37,9 @@ def predict_heuristic(x_mod, ut, clf, n_samples, rho, heuristic='uniform'):
     if heuristic == 'penalized_distance':
         pass
 
+def predict_heuristic_par(i,X,ut,clf,n_samples,rho,heuristic):
+    return predict_heuristic(X[i], ut, clf, n_samples, rho, heuristic='uniform')
+
 def predict_ARA(x_mod, N):
     '''
     Prediction using ARA
@@ -44,11 +49,21 @@ def predict_ARA(x_mod, N):
 
 if __name__ == '__main__':
 
-    X, y = get_spam_data("data/uciData.csv")
+    X, y = get_spam_data("../data/uciData.csv")
     clf = LogisticRegression()
     clf.fit(X,y)
     ut = np.array([[1,0], [0,1]])
     x = np.expand_dims(X[0], axis=0)
 
     print( predict_unaware(x, ut, clf) )
+
+    # Predict for one instance
     print(predict_heuristic(X[0], ut, clf, 10, 2, heuristic='uniform'))
+    # Predict for more instances
+    n_samples=10
+    rho=2
+    rows = 30 # predict for 10 samples
+    # rows = X.shape[0] # predict for all samples
+    num_cores=4 # it depends of the processor
+    vector_labels = Parallel(n_jobs=num_cores)(delayed(predict_heuristic_par)(i,X, ut, clf, n_samples, rho, heuristic='uniform') for i in range(rows))
+    print("Vector with label predictions: ", vector_labels)
